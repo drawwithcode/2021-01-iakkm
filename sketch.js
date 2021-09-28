b009e169d0ed3c1c2d8d26e185cb791612de6d8f
iakkm <90707822+iakkm@users.noreply.github.com>
2021-09-29 00:37:05 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,12 +2,65 @@ function preload() {
   // put preload code here
 }
 
+var detailX;
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  // put setup code here
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  angleMode(DEGREES);
+  frameRate(30);
+  detailX = createSlider(3, 24, 24);
+  detailX.position(windowWidth / 2.33, height - 40);
+  detailX.addClass("mySlider");
+  colorMode(HSB, 100);
 }
 
+let i = 0.0;
+let a = 1.0;
+let b = 0;
+
 function draw() {
-  // put drawing code here
-  background("red");
+  background(0);
+
+  stroke("white");
+  strokeWeight(1);
+  line(random(frameCount), -windowHeight, random(frameCount), windowHeight);
+  line(random(-frameCount), -windowHeight, random(-frameCount), windowHeight);
+  line(random(frameCount), -windowHeight, random(frameCount), windowHeight);
+  line(random(-frameCount), -windowHeight, random(-frameCount), windowHeight);
+  line(random(frameCount), -windowHeight, random(frameCount), windowHeight);
+  line(random(-frameCount), -windowHeight, random(-frameCount), windowHeight);
+  line(random(frameCount), -windowHeight, random(frameCount), windowHeight);
+  line(random(-frameCount), -windowHeight, random(-frameCount), windowHeight);
+
+  line(-windowHeight, random(frameCount), windowHeight, random(frameCount));
+  line(-windowHeight, random(-frameCount), windowHeight, random(-frameCount));
+  line(-windowHeight, random(frameCount), windowHeight, random(frameCount));
+  line(-windowHeight, random(-frameCount), windowHeight, random(-frameCount));
+  line(-windowHeight, random(frameCount), windowHeight, random(frameCount));
+  line(-windowHeight, random(-frameCount), windowHeight, random(-frameCount));
+  line(-windowHeight, random(frameCount), windowHeight, random(frameCount));
+  line(-windowHeight, random(-frameCount), windowHeight, random(-frameCount));
+
+  push();
+  rotateX(frameCount * 2);
+  rotateY(frameCount * 4);
+  rotateZ(frameCount);
+  torus(200, 25, detailX.value(), 14);
+  pop();
+
+  let myColor = lerpColor(color("orange"), color("violet"), float(i / 100));
+  stroke("black");
+  strokeWeight(0.5);
+  fill(myColor);
+
+  if (b <= 100) {
+    i = i + a;
+    if (i > 100) {
+      a = -1;
+    }
+    if (i < 0) {
+      a = 1.0;
+      b = b + 1;
+    }
+  }
 }
